e9f4914108b1a772211a537bd539c6f2b8a38579
Daniel Montenegro <dannielmdr7@gmail.com>
2024-05-09 15:00:16 -0500

    feature(messages-patterns): ProductsMessagesPatterns creado

---
+++ src/products-ms/messages-patterns/messages-patterns.ts
@@ -0,0 +1,7 @@
+export enum ProductsMessagesPatternsEnum {
+  CreateProduct = 'create-product',
+  DeleteProduct = 'delete-product',
+  GetProduct = 'get-product',
+  GetProducts = 'get-products',
+  UpdateProduct = 'update-product',
+}
